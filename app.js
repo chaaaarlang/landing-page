9af646193ad083fef11d7bd44725b1f2dd767a67
chaaaarlang <104968848+chaaaarlang@users.noreply.github.com>
2022-09-17 14:43:11 +0800

    files

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-/
+const sidebar = document.querySelector(".sidebar");
+const navBtn = document.querySelector(".nav-btn");
+navBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+});
